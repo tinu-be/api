ebddac2dfcafa6944f72e5c1d60aca6382a0d4eb
Bruno Rodrigues <brunowebr@gmail.com>
2020-01-25 00:17:46 -0300

    Add route to redirect short url

--- api/routes.js
+++ api/routes.js
@@ -53,5 +53,25 @@ router.post('/shorten', async (req, res) => {
     
 });
 
+//
+// GET
+// @route GET /:code
+// @description Redirect short url to original
+// 
+router.get('/:code', async (req, res) => {
+    try {
+        const url = await Url.findOne({ urlCode: req.params.code });
+
+        if(url) {
+            return res.redirect(url.longUrl);
+        } else {
+            return res.status(404).json('No url found');
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Server error');
+    }
+});
+
 module.exports = router;
 
