55ebc5d7c7d42668ba09147eaba271f6908d71d2
Bruno Rodrigues <brunowebr@gmail.com>
2020-01-25 00:14:30 -0300

    Add config to connect with database server

---
+++ api/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log('MongoDB Connected!');  
+    } catch(err) {
+        console.log(err.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.use(express.json({ extended: false }));
 app.use('/api', () => { return { is: true}});
