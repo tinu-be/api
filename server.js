8bf8737c9aa0f2445ec2e79b38177b69c01befc7
Bruno Rodrigues <brunowebr@gmail.com>
2020-01-28 14:14:11 -0300

    Add a option to custom root path

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(cors({ origin: process.env.CORS_ORIGIN }));
 // Connect Database
 connectDB();
 
+if(process.env.CUSTOM_ROOT_PATH) {
+    app.use('/', express.static(__dirname + process.env.CUSTOM_ROOT_PATH));
+}
+
 app.use(express.json({ extended: false }));
 app.use(routes);
 
